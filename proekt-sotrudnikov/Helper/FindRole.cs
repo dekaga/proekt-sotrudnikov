554167a0bd82a784cbe9a32544736350dc31faf3
у <@56_102>
2024-04-23 08:47:48 +0900

    Выполнена лабораторная работа номер 2. Создана папка Helper, в ней два класса, которые позволяют найти должность по коду

---
+++ proekt-sotrudnikov/Helper/FindRole.cs
@@ -0,0 +1,22 @@
+﻿using proekt_sotrudnikov.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proekt_sotrudnikov.Helper
+{
+    public class FindRole
+    {
+        int id;
+        public FindRole(int id)
+        {
+            this.id = id;
+        }
+        public bool RolePredicate(Role role)
+        {
+            return role.Id == id;
+        }
+    }
+}
